a5dd2833647772d1e41f9ca8b67843aa554e6f89
Corvino Francesco <corvinofrancesco@alice.it>
2012-06-22 11:16:10 +0200

    Implementation of randomParticlesTest
    Create two test to controll relations and particles sizes.

--- enterpriseGL-client/src/test/javascript/simulation/randomParticlesTest.js
+++ enterpriseGL-client/src/test/javascript/simulation/randomParticlesTest.js
@@ -1,6 +1,23 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+describe('Testing simulation', function(){
+    var  sSystem = new SimulSystem();
+    var sys = new ParticleSystem();
+    var numPar = 100, numRel = 120;
 
+    describe('Control particles creation', function(){
+        sSystem.createPart(sys,100);
 
+        it('Control number of particles created',function(){
+            expect(sys.size()).toBe(numPar);
+        });
+      
+    });
+    
+    describe('Control relations creation', function(){
+       sSystem.createRel(sys,numRel);
+       
+       it('Control number of relations created', function(){
+           expect(sys.relations.length).toBe(numRel);
+       })
+    });
+    
+});
