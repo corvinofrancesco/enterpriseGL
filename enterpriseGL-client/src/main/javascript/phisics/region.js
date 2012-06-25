438003980b6126f02d7eb3ecec801bf5ad80b04b
Sara Giacovelli <saragiacovelli@libero.it>
2012-06-25 15:58:10 +0200

    added insert method for BarnesHut algorithm with class test

--- enterpriseGL-client/src/main/javascript/phisics/region.js
+++ enterpriseGL-client/src/main/javascript/phisics/region.js
@@ -3,10 +3,18 @@
  * and open the template in the editor.
  */
 
-function Region(){
-    this.type = "spherical";
-    this.centre = new Particle();
+function Region(x, y, z){
+    this.type = "defcube";
+    this.centerx = x;
+    this.centery = y;
+    this.centerz = z;
     this.range = 10;
+    this.parent = null;
+    this.childs = [];
+    this.indexIds = {}; // gli indici devono essere unici
+    this.indexSize = [];
+    this.testVar = false;
+
 }
 
 Region.prototype = {
@@ -19,7 +27,7 @@ Region.prototype = {
      */
     contains : function (element) {
         var limit = this.range * this.range;
-        var dist = element.quadDistance(this.centre);
+        var dist = element.quadDistance({x:this.centerx, y:this.centery, z:this.centerz});
         if (this.type == "spherical") {
             return (limit == dist);
         } if(this.type == "defcube"){ // definite cube region
@@ -30,11 +38,71 @@ Region.prototype = {
     },
     
     move : function (x,y,z){
-        this.center.move(x,y,z);
+        this.centerx = x;
+        this.centery = y;
+        this.centerz = z;
     },
     
     resize : function (range) {
         this.range = range;
+    }, 
+    
+    forEach : function(func){        
+        this.childs.forEach(function(e,i,a){
+            func(e);
+        });
+    },
+    
+    add : function(n) {
+        this.childs.push(n);
+        var ElId = "unknow", ElSize = 0;
+        try {ElSize = n.size();} catch(e){};
+        try {ElId = n.id;} catch(e){};
+        
+        /// insert elements in the indexes
+        this.indexIds[ElId] = this.childs.length;
+        this.indexSize.push({size:ElSize, pos:this.childs.length});
+        
+        /// sorting indexes
+        //this.indexIds.sort( function(a,b){return a.id - b.id;});
+        this.indexSize.sort( function(a,b){return a.size - b.size;});
+
+    },
+    
+    getById : function(id){
+        if (this.contains(id)) 
+            return this.nodes[this.indexIds[id]];
+    },
+    
+    // range è il lato del cubo rappresentante la regione
+    insert: function(part, range){
+        var i=0, rx=0, ry=0, rz=0;
+        if(this.centerx < part.x) {
+            i = 1;
+            rx = range;
+        }
+        if(this.centery < part.y) {
+            i += 2;
+            ry = range;
+        }
+        if(this.centerz < part.z) {
+            i += 4;
+            rz = range;
+        }
+        
+        if(this.childs[i] == undefined){
+            this.childs[i] = part;
+            this.testVar = true;
+        } else if(this.childs[i] instanceof Region) {
+            this.childs[i].insert(part, 0.5*range);
+        } else {
+            var newRange = 0.5*range;
+            var newRegion = new Region(this.centerx - newRange + rx, this.centery - newRange + ry, 
+                this.centerz - newRange + rz);
+            newRegion.insert(part, newRange);
+            newRegion.insert(this.childs[i], newRange);
+            this.childs[i] = newRegion;
+        }
     }
     
 }
---
+++ enterpriseGL-client/src/test/javascript/phisics/RegionTest.js
@@ -0,0 +1,85 @@
+describe('Testing region', function(){
+    var p1 = new Particle(1);
+    var p2 = new Particle(2);
+    var p3 = new Particle(3);
+    var p4 = new Particle(4);
+    var p5 = new Particle(5);
+    var p6 = new Particle(6);
+    var p7 = new Particle(7);
+    var p8 = new Particle(8);
+    p1.move(15,15,15);
+    p2.move(12,8,8);
+    p3.move(8,12,8);
+    p4.move(12,12,8);
+    p5.move(8,8,12);
+    p6.move(12,8,12);
+    p7.move(8,12,12);
+    p8.move(12,12,12);
+    
+    describe('Initial number of childs', function(){
+        var r = new Region(9,4,4);
+        it('Control number of childs', function(){
+            expect(r.childs.length).toBe(0);
+        });
+        it('Control childs undefined', function(){
+            expect(r.testVar).toBe(false);
+        });        
+    });
+    
+    describe('Insert test of one particle', function(){
+        var r1 = new Region(20,20,20);
+       r1.insert(p1,40);
+       it('Control number of childs', function(){
+           expect(r1.childs.length).toBe(1);
+       });
+       it('Control childs undefined', function(){
+           expect(r1.testVar).toBe(true);
+       });        
+    });
+    
+//    describe('Final number of childs', function(){
+//        var r3 = new Region(10,10,10);        
+//       it('Control number of childs', function(){
+//           for(var i=0; i<8; i++) {
+//               var p = new Particle(i).move(5+i,5,8);
+//               r3.insert(p, 20);
+//                expect(r3.childs.length).toBe(i+1);             
+//           }
+//       });        
+//       it('Control childs undefined', function(){
+//           expect(r3.testVar).toBe(true);
+//       });        
+//    });
+    
+    describe('Control child', function(){
+        var r1 = new Region(20,20,20);
+       r1.insert(p1,40);       
+       it('Control child defined', function(){
+           expect(r1.childs[0]).not.toBe(undefined);
+       }); 
+       it('Control child position', function(){
+           expect(r1.childs[0].x).toBe(p1.x);
+       });        
+    });
+
+    describe('Control two childs', function(){
+        var r2 = new Region(10,10,10);
+       r2.insert(p2,20);       
+        r2.insert(p3,20);       
+       it('Control child defined', function(){
+           expect(r2.childs[1]).not.toBe(undefined);
+       }); 
+       it('Control child position', function(){
+           expect(r2.childs[1].x).toBe(p2.x);
+       });        
+
+       it('Control child defined', function(){
+           expect(r2.childs[2]).not.toBe(undefined);
+       }); 
+       it('Control child position', function(){
+           expect(r2.childs[2].x).toBe(p3.x);
+       });        
+
+    });
+       
+});
