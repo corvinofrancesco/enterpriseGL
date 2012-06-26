e5c2a664c227b9b4d432e469ec1e182630e44b58
Corvino Francesco <corvinofrancesco@alice.it>
2012-06-26 12:39:38 +0200

    Defined particlesystem properties

--- enterpriseGL-client/src/main/javascript/phisics/system.js
+++ enterpriseGL-client/src/main/javascript/phisics/system.js
@@ -1,12 +1,21 @@
 /**
+ * ParticleSystem types of event
+ */
+ParticleSystem.events = {
+    ADD: "add",
+    MODIFY: "modify",
+    REMOVE: "remove",
+    ERROR: "error"
+};
+
+/**
  * This class manage whole sistem of particles (interaction and forces)
  */
-
 function ParticleSystem(){
-    this.particles = [];
+    this.particles = {};
     this.relations = [];
-    this.textures = [];
-    this.cameraMatrix = null;
+    this.forces = {};
+    this.globalAlg = new BarnesHut();
 }
 
 ParticleSystem.prototype = {
@@ -16,68 +25,94 @@ ParticleSystem.prototype = {
     },
     
     updatePosition: function(dtime){
-        this.particles.forEach(function(p){
-          for(var axis in p.accelerations){
+        for(var i in this.particles){
+            var p = this.particles[i];
+            for(var axis in p.accelerations){
               p[axis] += 
                   0.5* dtime*dtime* p.accelerations[axis] +
                   dtime* p.velocity[axis];
               p.velocity[axis] += dtime * p.accelerations[axis];
-          }  
-        });
+            }  
+        }
     },
     
     /**
-     * Returns an Array of all particles vertex (particle as one point)
+     * This properties calculate all acceleration for particles in 
+     * the system.
      */
-    particlesVertex : function() {
-        var vertex = new Array();
-        this.particles.forEach(function(p){
-            vertex = vertex.concat(new Array(p.x,p.y,p.z));
-        });
-        return vertex;        
-    },
-    
-    particlesDirections: function() {
-        var directions = new Array();
-        this.particles.forEach(function(p){
-            directions = directions.concat(p.dirArray());
-        });
-        return directions;        
-        
-    },
-
-    particlesVelocities: function() {
-        var velocities = new Array();
-        this.particles.forEach(function(p){
-            velocities = velocities.concat(p.velArray());
-        });
-        return velocities;                
-    },
-    
-    particlesAccelerations: function() {
-        var accelerations = new Array();
-        this.particles.forEach(function(p){
-            accelerations = accelerations.concat(p.accArray());
-        });
-        return accelerations;                
-    },
-
-    particlesColors : function() {
-        var colors = new Array();
-        this.particles.forEach(function(p){
-            colors = colors.concat(p.colArray());
-        });
-        return colors;                
+    updateAccelerations : function() {
+        for(var findex in this.forces){
+            // funzione che calcola la forza da applicare
+            var force = this.forces[findex].force;
+            // seleziona le relazioni su cui applicare la forza
+            var selector = this.forces[findex].selector;   
+            switch(this.forces[findex].type) {
+                case Force.types.GLOBAL:
+                    // configura l'algoritmo globale 
+                    // con i parametri della forza
+                    this.globalAlg.configureFor(
+                        this.forces[findex]);
+                    // aggiunge il calcolo delle forze
+                    for(var i in this.particles)
+                        this.globalAlg.getForceFor(this.particles[i]);
+                    break;
+                case Force.types.ONRELATIONS:
+                    for(var r in this.relations.filter(selector)){
+                        force(this.relations[r]);
+                    }
+                    break;
+                case Force.types.LOCAL:                
+                default:
+                    for(var i in this.particles.filter(selector)){
+                        force(this.particles[i]);
+                    }
+                break;                    
+            }
+        }
     },
     
     /**
-     * Returns an Array of all particles relations
+     * Aggiunge una particella al sistema.
+     * La particella avrà nell'array particles id analogo al suo valore in modo
+     * da essere recuperata.
+     * Per essere modificata basta richiamare lo stesso metodo.
      */
-    particlesEdges : function() {
-        var edges = new Array();
-        this.relations.forEach(function(r){
-            edges = edges.concat(r.getArray());
-        });
-        return edges;                
+    add : function(p, relations) {
+        var genEvent = ParticleSystem.events.ADD;
+        if(this.particles[p.id] != undefined) {
+            genEvent = ParticleSystem.events.MODIFY;
+            
+        } else {
+            // TODO effettuare l'insert nell'algoritmo globale
+            this.globalAlg.insert(p);
+        }
+        this.particles[p.id] = p;
+        if(relations) {
+            this.relations = this.relations.concat(relations);
+        }
+        // TODO make something with globalAlg
+        this.event(genEvent, {particle:p});
+    },
+    
+    /**
+     * Si occupa di rimuovere una particella dal sistema,
+     * Se la particella non esiste genera un evento ERROR
+     */
+    remove : function(id) {
+        if(this.particles[id]) {
+            var p = this.particles[id];
+            this.particles[id] = undefined;
+            // TODO make something with globalAlg
+            this.event(ParticleSystem.events.REMOVE, {particle:p})
+        }
+        this.event(ParticleSystem.events.ERROR,{particle:p});
+    },
+    
+    /**
+     * Manage 
+     */
+    event : function(type, props) {
+        // TODO make something
     }
+    
 }
