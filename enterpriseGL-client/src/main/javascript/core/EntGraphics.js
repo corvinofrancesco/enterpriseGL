703403a830b3f33f0aed158e055f72bf4cf1ddc6
Corvino Francesco <corvinofrancesco@alice.it>
2012-06-29 09:43:08 +0200

    Add load function to EntGraphics class

--- enterpriseGL-client/src/main/javascript/core/EntGraphics.js
+++ enterpriseGL-client/src/main/javascript/core/EntGraphics.js
@@ -12,6 +12,18 @@ function EntGraphics() {
 }
 
 EntGraphics.prototype = {
+    load : function(gl,reqManager){
+        for(var i in this.primitives){
+            // load programs
+            var shVsrc = sglNodeText(this.primitives[i].idVShaderSrc);
+            var shFsrc = sglNodeText(this.primitives[i].idFShaderSrc);
+            var program = new SglProgram(gl,[shVsrc],[shFsrc]);
+            this.primitives[i].program = program;
+            /// carica i modelli delle primitive
+            this.primitives[i].load(gl,reqManager);            
+        }
+        
+    }
     
 }
 
