e8c97b9a081ede6fd6871ddb6d5f3ead4a2f5462
Corvino Francesco <corvinofrancesco@alice.it>
2012-06-29 13:12:11 +0200

    Draw axis

--- enterpriseGL-client/src/main/javascript/core/EntGraphics.js
+++ enterpriseGL-client/src/main/javascript/core/EntGraphics.js
@@ -48,6 +48,31 @@ EntGraphics.prototype = {
           context.xform.model.pop();
         }
         
+            var sys = new ParticleSystem();
+            sys.add((new Particle(0)),null);
+            sys.add((new Particle(1)).move(1,0,0),null);
+            sys.add((new Particle(2)).move(0,1,0),null);
+            sys.add((new Particle(3)).move(0,0,1),null);
+            sys.add((new Particle(4)).move(0,0,2),null);
+            sys.add((new Particle(5)).move(1,1,0),null);
+            sys.relations = [
+                new Relation(0,1),
+                new Relation(0,2),
+                new Relation(0,3),
+                new Relation(3,4),
+                new Relation(2,5)
+            ];   
+        for(var i in sys.relations) {
+          var r = sys.relations[i];
+          var particles = sys.particles;
+          r.source = particles[r.idS];
+          r.destination = particles[r.idD];
+          context.xform.model.push();
+          context.xform.model.loadIdentity();
+          this.primitives.baseRelation.draw(gl,context,r);
+          context.xform.model.pop();
+        }
+          
     }
     
 }
