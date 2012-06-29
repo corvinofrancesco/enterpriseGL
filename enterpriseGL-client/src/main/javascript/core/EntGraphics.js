edc4239c41c83b5029e7c76300485339e33cb174
Corvino Francesco <corvinofrancesco@alice.it>
2012-06-29 09:03:40 +0200

    Delete from EntGraphics old class ParticlePrimitive

--- enterpriseGL-client/src/main/javascript/core/EntGraphics.js
+++ enterpriseGL-client/src/main/javascript/core/EntGraphics.js
@@ -6,87 +6,12 @@ function EntGraphics() {
     this.psystem = new ParticleSystem();  
     this.modelLoader.popolate(this.psystem);
     this.primitives = {
-        starParticles : new ParticlePrimitive()
+        starParticles : new ParticleCube()
         
     };
 }
 
 EntGraphics.prototype = {
-    
-}
-
-/**
- * Class for particle graphics element
- */
-function ParticlePrimitive(program) {
-    // inizializza con texture non valida
-    this.texture = {isValid: false};
-    // programma di rappresentazione della texture
-    this.textureShaders = program;   
-}
-
-ParticlePrimitive.prototype = {
-    
-    /**
-     * Carica la texture della tipologia di particella
-     * @param gl oggetto rappresentante l'interfaccia webgl
-     * @param reqManager oggetto che gestisce il canvas
-     */
-    load: function(gl,reqManager){
-        var texOpt = {
-                generateMipmap : true,
-                minFilter      : gl.LINEAR_MIPMAP_LINEAR,
-                onload         : reqManager.ui.requestDraw
-        };
-        var tex = new SglTexture2D(gl, "star.gif", texOpt);
-        this.texture = tex;
-    },
-    
-    /**
-     * Disegna una particella 
-     * @param 
-     */
-    draw: function(gl,t,p){
-        // we don't have texture to display
-        if(!this.texture.isValid) return;
-        
-        /*************************************************************/
-        var vp = vec3.create([p.x,p.y,p.z]);
-        mat4.multiplyVec3( t.xform.modelViewProjectionMatrix, vp);
-
-        //log(t.xform.modelViewProjectionMatrix,"DRAW",true);
-        //log(vec3.str(vp) + ": " + "("+p.x+"," +p.y+","+p.z+")","DRAW",true);
-
-        var quadPositions = new Float32Array ([
-                vp[0]-1.0, vp[1]-1.0, vp[2],
-                vp[0]+1.0, vp[1]-1.0, vp[2],
-                vp[0]-1.0, vp[1]+1.0, vp[2],
-                vp[0]+1.0, vp[1]+1.0, vp[2]
-        ]);
-        
-        var quadTexcoords = new Float32Array ([
-                0.0, 0.0,
-                1.0, 0.0,
-                0.0, 1.0,
-                1.0, 1.0
-        ]);
-
-        var quad = new SglMeshGL(gl);
-        quad.addVertexAttribute("position", 3, quadPositions);
-        quad.addVertexAttribute("texcoord", 2, quadTexcoords);
-        quad.addArrayPrimitives("tristrip", gl.TRIANGLE_STRIP, 0, 4);
-        this.quadMesh = quad;
-        /*************************************************************/
-        
-            var quadUniforms = {u_mvp : t.xform.modelViewProjectionMatrix};
-            var quadSamplers = {s_texture : this.texture};
-            sglRenderMeshGLPrimitives(this.quadMesh, "tristrip", 
-                    this.textureShaders, null, quadUniforms, quadSamplers);            
-    },
-    
-    animate: function(){
-        
-    }
     
 }
 
