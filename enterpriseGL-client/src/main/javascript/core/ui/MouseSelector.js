b53a6d117e7b1911f423ccc99135083dceb71fbf
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-02 20:39:44 +0200

    Add comments to MouseSelector.js

--- enterpriseGL-client/src/main/javascript/core/ui/MouseSelector.js
+++ enterpriseGL-client/src/main/javascript/core/ui/MouseSelector.js
@@ -1,6 +1,7 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/** 
+ * This class manage the input of mouse on graphic element
+ * @param camera object with information of current position of point of view
+ * @param plane graphical object with 
  */
 function MouseSelector(camera,plane,objects,w,h){
     this.camera = camera;
@@ -14,11 +15,16 @@ function MouseSelector(camera,plane,objects,w,h){
 }
 
 MouseSelector.prototype = {
+    /**
+     * Update the position of mouse
+     * @param domx x position of mouse in the document
+     * @param domy y position of mouse in the document
+     */
     update: function (domx,domy){
         var x = ( domx / this.width ) * 2 - 1;
         var y =  - ( domy / this.height ) * 2 + 1;;
         
-        var vector = new THREE.Vector3( x, y, 0.5 );
+        var vector = new THREE.Vector3( x, y, 1 );
         this.projector.unprojectVector( vector, this.camera );
 
         this.ray = new THREE.Ray( 
@@ -27,12 +33,18 @@ MouseSelector.prototype = {
         );
     },
     
+    /**
+     * Returns all objects intersected by the mouse 
+     */
     getObjects : function(){
         if(this.ray==null) return null;
         var intersects = this.ray.intersectObjects( this.objects );
         return intersects;
     },
     
+    /**
+     * Return the intersection of mouse with the plane
+     */
     getPlane : function() {
         if(this.ray==null) return null;
         var intersects = this.ray.intersectObject( this.plane );
