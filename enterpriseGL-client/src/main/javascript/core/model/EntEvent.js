868fc1fbdb37d55de82857b3c3f549f94307f52a
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-04 19:26:25 +0200

    Create class EntEvent

---
+++ enterpriseGL-client/src/main/javascript/core/model/EntEvent.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+function EntEvent(){
+    EntObject.call(this);
+    this.graphicalModel = null;
+    this.title = "event";
+    this.date = new Date();
+    this.objects = [];
+}
+
+EntEvent.prototype = new EntObject();
+EntEvent.prototype.constructor = EntEvent;
+
+EntEvent.prototype.getDescription = function(){
+    var text = "";
+    text += "<h1>" + this.date + ": " + this.title +"</h1>";
+    text += "<p>" + this.description + "</p>";
+    if(this.relations.length>0){
+        text += "<ul>";
+        for(var i in this.objects){
+            var e = EntObjects.instance.generateLink(this.objects[i]);
+            text += "<li>" + e + "</li>";
+        }        
+        text += "</ul>";
+    }
+    return text;
+}
+
+EntEvent.prototype.setProperties = function(prop){
+    this.setId(prop.id);
+    this.title = prop.nametime || this.title;
+    this.date = prop.date || this.date;
+    this.description = prop.description;
+    this.objects = prop.objects;
+}
