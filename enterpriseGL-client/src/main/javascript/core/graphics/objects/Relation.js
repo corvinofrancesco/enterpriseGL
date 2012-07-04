ffb45301267f967eda69f7285d1a125fb941f7ef
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-04 12:57:40 +0200

    Complete setExtremis function for Relation class

--- enterpriseGL-client/src/main/javascript/core/graphics/objects/Relation.js
+++ enterpriseGL-client/src/main/javascript/core/graphics/objects/Relation.js
@@ -13,9 +13,7 @@ Relation.prototype =  {
             prop.material = new THREE.LineBasicMaterial( color );
         }
         var object = new THREE.Line(geom, prop.material);
-        object.setExtremis = function (p1,p2){
-            this.value = "a";
-        };
+        object.setExtremis = Relation.setExtremis;
         return object;
     },
 
@@ -24,5 +22,18 @@ Relation.prototype =  {
         lineGeometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
         lineGeometry.vertices.push( new THREE.Vector3( 0, 1, 0 ) );
         return lineGeometry;
-    },
+    }
 }
+
+Relation.setExtremis = function(p1,p2){
+    this.position = new THREE.Vector3( p1.x, p1.y, p1.z );
+    var diff = new THREE.Vector3(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
+    var length = diff.length();
+    var dir = diff.normalize();
+    var axis = new THREE.Vector3(0,1,0).crossSelf(dir);
+    var radians = Math.acos( new THREE.Vector3( 0, 1, 0 ).dot( dir.clone().normalize() ) );
+
+    this.matrix = new THREE.Matrix4().makeRotationAxis( axis.normalize(), radians );
+    this.rotation.getRotationFromMatrix( this.matrix, this.scale );
+    this.scale.set( length, length, length );
+};
