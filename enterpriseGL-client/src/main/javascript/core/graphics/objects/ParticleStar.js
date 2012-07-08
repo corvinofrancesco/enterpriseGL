1666450fe57d4addd05158b82bce925caa02ccde
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-08 21:21:07 +0200

    Update ParticleStar

--- enterpriseGL-client/src/main/javascript/core/graphics/objects/ParticleStar.js
+++ enterpriseGL-client/src/main/javascript/core/graphics/objects/ParticleStar.js
@@ -2,79 +2,34 @@
  * Draw a Star Particle
  */
 function ParticleStar(){
-    /// shader configurations
-    this.idVShaderSrc = "TEX_VERTEX_SHADER";
-    this.idFShaderSrc = "TEX_FRAGMENT_SHADER";
-    this.program = null;
-    // inizializza con texture non valida
-    this.texture = {isValid: false};
+    this.geometry = new THREE.Geometry();
+    this.geometry.vertices.push(new THREE.Vector3(0,0,0));
 }
 
 ParticleStar.prototype = {
-    
-    /**
-     * Carica la texture della tipologia di particella
-     * @param gl oggetto rappresentante l'interfaccia webgl
-     * @param reqManager oggetto che gestisce il canvas
-     */
-    load: function(gl,reqManager){
-        var texOpt = {
-                generateMipmap : true,
-                minFilter      : gl.LINEAR_MIPMAP_LINEAR,
-                onload         : reqManager.ui.requestDraw
-        };
-        var tex = new SglTexture2D(gl, "star.gif", texOpt);
-        this.texture = tex;
-        /*************************************************************/
-        var quadPositions = new Float32Array ([
-                -1.0, -1.0, 0,
-                +1.0, -1.0, 0,
-                -1.0, +1.0, 0,
-                +1.0, +1.0, 0
-        ]);
-        
-        var quadTexcoords = new Float32Array ([
-                0.0, 0.0,
-                1.0, 0.0,
-                0.0, 1.0,
-                1.0, 1.0
-        ]);
+    generator: function(geom, prop){
+        var sprite = THREE.ImageUtils.loadTexture( "ball.png" );
+//        var sprite = THREE.ImageUtils.loadTexture( "snowflake1.png" );
+        var color = [0.2,0.5,0.9];
+        var size = 20;
+        var material = new THREE.ParticleBasicMaterial({
+            size: size,
+            map: sprite,
+            blending: THREE.AdditiveBlending,
+            depthTest: false,
+            transparent : false 
+        });
+        material.color.setHSV(
+            color[0],
+            color[1],
+            color[2]);
+        var geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3(0,0,0));
+        var p = new THREE.ParticleSystem(geometry,material); 
+        p.constructor = THREE.Particle;
+        p.scale = new THREE.Vector3(1,1,1);
+//        var p = new THREE.Particle(material); 
 
-        var quad = new SglMeshGL(gl);
-        quad.addVertexAttribute("position", 3, quadPositions);
-        quad.addVertexAttribute("texcoord", 2, quadTexcoords);
-        quad.addArrayPrimitives("tristrip", gl.TRIANGLE_STRIP, 0, 4);
-        this.quadMesh = quad;
-        /*************************************************************/
-
-    },
-    
-    /**
-     * Disegna una particella 
-     * @param gl webgl interface
-     * @param t context 
-     * @param p particle to draw
-     */
-    draw: function(gl,t,p){
-        // we don't have texture to display
-        if(!this.texture.isValid) return;        
-
-        gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-        gl.enable(gl.BLEND);
-
-        t.xform.model.translate(p.x,p.y,p.z);
-        var quadUniforms = {u_mvp : t.xform.modelViewProjectionMatrix};
-        var quadSamplers = {s_texture : this.texture};
-        sglRenderMeshGLPrimitives(this.quadMesh, "tristrip", 
-                this.program, null, quadUniforms, quadSamplers);            
-        
-        gl.disable(gl.BLEND);
-
-    },
-    
-    animate: function(){
-        
+        return p;
     }
-    
-    
 }
