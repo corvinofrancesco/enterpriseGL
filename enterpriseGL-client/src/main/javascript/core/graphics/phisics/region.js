2fada243bbe2f95653f406f3fe9b301a35af37eb
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-09 16:38:58 +0200

    Updating region class

--- enterpriseGL-client/src/main/javascript/core/graphics/phisics/region.js
+++ enterpriseGL-client/src/main/javascript/core/graphics/phisics/region.js
@@ -10,10 +10,6 @@ function Region(x, y, z){
     this.range = 10;
     this.parent = null;
     this.childs = [];
-    this.indexIds = {}; // gli indici devono essere unici
-    this.indexSize = [];
-    this.testVar = false;
-
 }
 
 Region.prototype = {
@@ -50,57 +46,30 @@ Region.prototype = {
         });
     },
     
-    add : function(n) {
-        this.childs.push(n);
-        var ElId = "unknow", ElSize = 0;
-        try {ElSize = n.size();} catch(e){};
-        try {ElId = n.id;} catch(e){};
-        
-        /// insert elements in the indexes
-        this.indexIds[ElId] = this.childs.length;
-        this.indexSize.push({size:ElSize, pos:this.childs.length});
-        
-        /// sorting indexes
-        //this.indexIds.sort( function(a,b){return a.id - b.id;});
-        this.indexSize.sort( function(a,b){return a.size - b.size;});
-
-    },
-    
-    getById : function(id){
-        if (this.contains(id)) 
-            return this.nodes[this.indexIds[id]];
-    },
-    
     /**
      * Inserisce nella regione this la particella 
      * passata con il parametro part 
      */ 
     insert: function(part){
-        var i=0, rx=0, ry=0, rz=0;
+        var i=0, r = new THREE.Vector3(0,0,0);
         if(this.position.x < part.position.x) {
-            i = 1;
-            rx = 2 * this.range;
+            i = 1; r.setX(2 * this.range);
         }
         if(this.position.y < part.position.y) {
-            i += 2;
-            ry = 2 * this.range;
+            i += 2; r.setY(2 * this.range);
         }
         if(this.position.z < part.position.z) {
-            i += 4;
-            rz = 2 * this.range;
+            i += 4; r.setZ(2 * this.range);
         }
         
-        if(this.childs[i] == undefined){
-            this.childs[i] = part;
-            this.testVar = true;
-        } else if(this.childs[i] instanceof Region) {
+        if(this.childs[i] == undefined) this.childs[i] = part;
+        else if(this.childs[i] instanceof Region) {
             this.childs[i].insert(part, this.range);
         } else {
             var newRange = this.range;
-            var newRegion = new Region(
-                this.position.x - newRange + rx, 
-                this.position.y - newRange + ry, 
-                this.position.z - newRange + rz);
+            var newRegion = new Region();
+            newRegion.position = this.position.clone()
+                .addScalar(-newRange).addSelf(r);
             newRegion.range = 0.5 * newRange;
             newRegion.insert(part, newRange);
             newRegion.insert(this.childs[i], newRange);
@@ -109,30 +78,27 @@ Region.prototype = {
     }, 
     
     computeCenterOfMass: function(){
-        var mass=0, m=0,
+        var mass=0,
             p = new THREE.Vector3(0,0,0);
         this.childs.forEach(function(ch){
-            if (ch!=null) {
-                if(ch instanceof Region){
-                    ch = ch.computeCenterOfMass();                    
-                } else {
-                    m = ch.mass;
-                    mass += m;
-                    p.addSelf(ch.position.clone().multiplyScalar(m));
-                }
+            if(ch instanceof Region){
+                ch = ch.computeCenterOfMass();                    
+            } else {
+                var m = ch.mass;
+                mass += m;
+                p.addSelf(ch.position
+                    .clone().multiplyScalar(m));
             }
-        });
-        
-        m = 1.0 / mass;
+        });        
         // distinguere il centro della regione dal centro di massa??
-        this.position = p.multiplyScalar(m);
+        this.position = p.multiplyScalar(1.0 / mass);
         this.mass = mass;
         
         return {mass:mass, x:this.position.x, y:this.position.y, z:this.position.z};
     },
     
     getMass : function(){
-        
+        this.comuteCenterOfMass();
     }
     
 }
--- enterpriseGL-client/src/test/javascript/core/graphics/phisics/RegionTest.js
+++ enterpriseGL-client/src/test/javascript/core/graphics/phisics/RegionTest.js
@@ -1,95 +1,29 @@
-describe('Testing region', function(){
-    var p1 = new Particle(1);
-    var p2 = new Particle(2);
-    var p3 = new Particle(3);
-    var p4 = new Particle(4);
-    var p5 = new Particle(5);
-    var p6 = new Particle(6);
-    var p7 = new Particle(7);
-    var p8 = new Particle(8);
-    p1.move(15,15,15);
-    p2.move(12,8,8);
-    p2.mass = 4;
-    p3.move(8,12,8);
-    p3.mass = 4;
-    p4.move(12,12,8);
-    p5.move(8,8,12);
-    p6.move(12,8,12);
-    p7.move(8,12,12);
-    p8.move(12,12,12);
+describe('Region Testing', function(){
+    var p = [
+        {position: new THREE.Vector3(15,15,15)},
+        {position: new THREE.Vector3(12, 8, 8), getMass:function(){return 4;}},
+        {position: new THREE.Vector3( 8,12, 8), getMass:function(){return 4;}},
+        {position: new THREE.Vector3(12,12, 8)},
+        {position: new THREE.Vector3( 8, 8,12)},
+        {position: new THREE.Vector3(12, 8,12)},
+        {position: new THREE.Vector3( 8,12,12)},
+        {position: new THREE.Vector3(12,12,12)}
+    ];
     
-    describe('Initial number of childs', function(){
+    describe('Testing constutor', function(){
         var r = new Region(9,4,4);
         it('Control number of childs', function(){
             expect(r.childs.length).toBe(0);
         });
-        it('Control childs undefined', function(){
-            expect(r.testVar).toBe(false);
-        });        
+       
     });
     
-    describe('Insert test of one particle', function(){
+    describe('insert() Test', function(){
         var r1 = new Region(20,20,20);
-       r1.insert(p1,40);
-       it('Control number of childs', function(){
+        r1.insert(p[0],40);
+        it('Control number of childs', function(){
            expect(r1.childs.length).toBe(1);
-       });
-       it('Control childs undefined', function(){
-           expect(r1.testVar).toBe(true);
-       });        
-    });
-    
-//    describe('Final number of childs', function(){
-//        var r3 = new Region(10,10,10);        
-//       it('Control number of childs', function(){
-//           for(var i=0; i<8; i++) {
-//               var p = new Particle(i).move(5+i,5,8);
-//               r3.insert(p, 20);
-//                expect(r3.childs.length).toBe(i+1);             
-//           }
-//       });        
-//       it('Control childs undefined', function(){
-//           expect(r3.testVar).toBe(true);
-//       });        
-//    });
-    
-    describe('Control child', function(){
-        var r1 = new Region(20,20,20);
-       r1.insert(p1,40);       
-       it('Control child defined', function(){
-           expect(r1.childs[0]).not.toBe(undefined);
-       }); 
-       it('Control child position', function(){
-           expect(r1.childs[0].x).toBe(p1.x);
-       });        
-    });
-
-    describe('Control two childs', function(){
-        var r2 = new Region(10,10,10);
-        r2.insert(p2,20);       
-        r2.insert(p3,20);       
-        it('Control child defined', function(){
-            expect(r2.childs[1]).not.toBe(undefined);
-        }); 
-        it('Control child position', function(){
-            expect(r2.childs[1].x).toBe(p2.x);
-        });        
-
-        it('Control child defined', function(){
-             expect(r2.childs[2]).not.toBe(undefined);
-        }); 
-        it('Control child position', function(){
-            expect(r2.childs[2].x).toBe(p3.x);
-        });  
-        it('Control center of mass', function(){
-            var rr = r2.computeCenterOfMass();
-            expect(rr.mass).toBe(8);
-            expect(rr.x).toBe(10);
-            expect(rr.y).toBe(10);
-            expect(rr.z).toBe(8);
-        });  
-        
-
+        });
     });
        
 });
