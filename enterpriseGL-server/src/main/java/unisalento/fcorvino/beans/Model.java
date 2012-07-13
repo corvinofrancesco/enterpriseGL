1d17b06b9c48069f4b6cc7fea8480edc7dbbb5cc
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-13 20:26:15 +0200

    Adding properties to Model class

--- enterpriseGL-server/src/main/java/unisalento/fcorvino/beans/Model.java
+++ enterpriseGL-server/src/main/java/unisalento/fcorvino/beans/Model.java
@@ -1,18 +1,28 @@
 package unisalento.fcorvino.beans;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import unisalento.fcorvino.beans.models.ModelTable;
 
 /**
  *
  * @author Francesco Corvino
  */
 public class Model {
+    
+    public enum ModelStatus {
+        Complete, Incomplete
+    };
+    
     private TreeSet<Particle> particles;
     private List relations;
     private String name;
+    private ModelTable typeModel;
+    private Date lastChange;
+    private ModelStatus status;
     
     public Model(){
         this.particles = new TreeSet();
@@ -46,6 +56,30 @@ public class Model {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Date getLastChange() {
+        return lastChange;
+    }
+
+    public void setLastChange(Date lastChange) {
+        this.lastChange = lastChange;
+    }
+
+    public ModelStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ModelStatus status) {
+        this.status = status;
+    }
+
+    public ModelTable getTypeModel() {
+        return typeModel;
+    }
+
+    public void setTypeModel(ModelTable typeModel) {
+        this.typeModel = typeModel;
     }
 
     @Override
