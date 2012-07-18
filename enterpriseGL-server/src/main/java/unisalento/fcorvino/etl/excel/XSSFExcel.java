d2a4eaed6de943ddab72b48d2e8f604f6c8dfe58
Sara Giacovelli <saragiacovelli@libero.it>
2012-07-18 11:22:36 +0200

    added code for detecting date type in excel cells

--- enterpriseGL-server/src/main/java/unisalento/fcorvino/etl/excel/XSSFExcel.java
+++ enterpriseGL-server/src/main/java/unisalento/fcorvino/etl/excel/XSSFExcel.java
@@ -9,6 +9,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import unisalento.fcorvino.etl.EtlLoadBean.EtlLoadTypes;
 
@@ -43,9 +44,21 @@ public class XSSFExcel extends Excel {
         Cell xssfcell = (Cell) cell;
         EtlLoadTypes type = null;
         switch(xssfcell.getCellType()){
-            case Cell.CELL_TYPE_NUMERIC:
-            case Cell.CELL_TYPE_FORMULA: return EtlLoadTypes.EtlNumeric;
+            case Cell.CELL_TYPE_NUMERIC: 
+                if(DateUtil.isCellDateFormatted(xssfcell))
+                    return EtlLoadTypes.EtlData;
+                else return EtlLoadTypes.EtlNumeric;
             case Cell.CELL_TYPE_STRING: return EtlLoadTypes.EtlText;
+            case Cell.CELL_TYPE_FORMULA: 
+            switch (xssfcell.getCachedFormulaResultType()){
+                case Cell.CELL_TYPE_STRING:
+                    return EtlLoadTypes.EtlText;
+                case Cell.CELL_TYPE_NUMERIC:
+                    if(DateUtil.isCellDateFormatted(xssfcell))
+                        return EtlLoadTypes.EtlData;
+                    else return EtlLoadTypes.EtlNumeric;
+                    default:
+            }
         }
         return type;
     }
