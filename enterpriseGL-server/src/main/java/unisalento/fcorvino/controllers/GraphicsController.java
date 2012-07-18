376fdc3a1d58073b90ad5ca8d11c4f354765d206
Corvino Francesco <corvinofrancesco@alice.it>
2012-07-18 14:33:50 +0200

    Add Search and Graphics Controller

---
+++ enterpriseGL-server/src/main/java/unisalento/fcorvino/controllers/GraphicsController.java
@@ -0,0 +1,14 @@
+package unisalento.fcorvino.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Francesco Corvino
+ */
+@Controller
+@RequestMapping(value="/graphics")
+public class GraphicsController {
+    
+}
---
+++ enterpriseGL-server/src/main/java/unisalento/fcorvino/controllers/SearchController.java
@@ -0,0 +1,14 @@
+package unisalento.fcorvino.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Francesco Corvino
+ */
+@Controller
+@RequestMapping(value="/search")
+public class SearchController {
+    
+}
