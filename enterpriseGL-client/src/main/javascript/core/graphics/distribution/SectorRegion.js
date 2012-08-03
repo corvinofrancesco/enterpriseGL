f4617029610fe42e064db511d96262683c21be1d
Corvino Francesco <corvinofrancesco@alice.it>
2012-08-03 23:34:05 +0200

    Correct error in SectorRegion

--- enterpriseGL-client/src/main/javascript/core/graphics/distribution/SectorRegion.js
+++ enterpriseGL-client/src/main/javascript/core/graphics/distribution/SectorRegion.js
@@ -1,10 +1,10 @@
-EntGL.SectorRegion.prototype = new Region(0,0,0)
-EntGL.SectorRegion.constructor = EntGl.SectorRegion;
-EntGL.SectorRegion.superclass = Region.prototype
-
 EntGL.SectorRegion = function(){
     
 }
+
+EntGL.SectorRegion.prototype = new Region(0,0,0)
+EntGL.SectorRegion.constructor = EntGL.SectorRegion;
+EntGL.SectorRegion.superclass = Region.prototype
 
 EntGL.SectorRegion.prototype.init = function(quadrant,l1,l2){
     this.index = quadrant;
